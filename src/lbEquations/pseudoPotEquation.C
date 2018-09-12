10bdf99a96ddb0ba8d50a25def8d74bce42b9090
Ezequiel Fogliatto <ezefogliatto@gmail.com>
2018-09-12 15:45:57 -0300

    Fuerzas en pseudoPotEquation

--- src/lbEquations/pseudoPotEquation.C
+++ src/lbEquations/pseudoPotEquation.C
@@ -66,7 +66,122 @@ const void pseudoPotEquation::localVelocity( std::vector<scalar>& v, const uint&
 
 
 
-/** Default constructor */
+
+
+
+
+/** Interaction force field */
+
+const void pseudoPotEquation::updateIntForce() {
+
+
+    // Reference to neighbour array
+
+    const vector< vector<int> > nb = mesh.nbArray();
+
+
+    // Lattice model properties
+
+    vector< vector<int> > vel = mesh.lmodel()->lvel();
+
+    vector<uint> reverse = mesh.lmodel()->reverse();
+
+    scalar cs2 = mesh.lmodel()->cs2();
+
+    
+    
+    
+
+    for( uint id = 0 ; id < mesh.local() ; id++ ) {
+    
+	vector<scalar> F = {0.0, 0.0, 0.0};
+
+	uint noneigh(0);
+    
+
+	// Move over neighbours and check for boundary
+	for( uint k = 1 ; k < _q ; k++ ) {
+
+		if( nb[id][k] == -1 ) {
+	    
+		    noneigh++;
+	    
+		}
+
+	}
+
+
+	// Do not use unexisting neighbour
+	if( noneigh == 0 ) {
+
+	
+		for( uint k = 1 ; k < _q ; k++ ) {
+       
+
+		    int neighId = nb[id][ reverse[k] ];
+
+		    scalar nbrho = rho[neighId];
+
+		    scalar nbt = T[neighId];
+	    
+		    scalar alpha = weights[k] * eos->potential( eos->p_eos(nbrho, nbt), cs2, nbrho, nbt );
+
+	    
+		    for( uint i = 0 ; i < 3 ; i++ ) {
+
+		    	F[i] +=  alpha * (scalar)vel[k][i] ;
+
+		    }
+    
+
+		}
+
+	
+
+	
+
+		// Extra constant
+		
+		scalar beta = -eos->G() * eos->potential( eos->p_eos(rho[id], T[id]), cs2, rho[id], T[id] );
+    
+		for( uint i = 0 ; i < 3 ; i++) {
+	
+		    Fi[id][i] =  F[i] * beta;
+	
+		}	
+
+		
+	}
+
+    }
+
+}
+
+
+    
+/** Total force field */
+
+const void pseudoPotEquation::updateTotalForce() {
+
+    for( uint i = 0 ; i < mesh.local() ; i++ ) {
+    
+	for( uint j = 0 ; j < 3 ; j++) {
+	
+	    Ft[i][j] = Fi[i][j] +  (rho[i] - rho_0) * gravity[j];
+	
+	}
+
+    }
+
+
+}
+
+
+
+
+
+
+/** Constructor */
 
 pseudoPotEquation::pseudoPotEquation( const string& name,
 				      const latticeMesh& mesh_,
@@ -87,6 +202,41 @@ pseudoPotEquation::pseudoPotEquation( const string& name,
     EOSCreator creator;
 
     eos = creator.create("properties/macroProperties");
+
+    
+
+    // Read gravity field and ext fixed force
+
+    dictionary dict("properties/macroProperties");
+
+    gravity  = dict.lookUp< vector<scalar> >("Navier-Stokes/gravity");
+
+    extForce = dict.lookUp< vector<scalar> >("Navier-Stokes/extForce");
+
+
+    
+    // Create pp weights array
+
+    if(   ( mesh.lmodel()->d() == 2 )  &&  ( mesh.lmodel()->q() == 9 )   ) {
+
+	weights.push_back( 0 );
+	weights.push_back( 1.0/3 );
+	weights.push_back( 1.0/3 );
+	weights.push_back( 1.0/3 );
+	weights.push_back( 1.0/3 );
+	weights.push_back( 1.0/12 );
+	weights.push_back( 1.0/12 );
+	weights.push_back( 1.0/12 );
+	weights.push_back( 1.0/12 );	
+
+    }
+
+    else {
+
+	cout << " [ERROR]  Pseudopotential model not yet implemented" << endl << endl;
+
+    }
+    
 
 }
 
