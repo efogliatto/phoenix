9db516fc7e391a900beed6c70cb6faaa87f065b0
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-09-22 15:07:40 -0300

    Incorporaci\'on de un handler para ecuaci\'on de enrg\'ia

---
+++ src/lbEquations/energy/energyEqHandler.C
@@ -0,0 +1,48 @@
+#include <energyEqHandler.H>
+
+using namespace std;
+
+
+/** Constructor */
+
+energyEqHandler::energyEqHandler(const std::string& name,
+				 const latticeMesh& mesh_,
+				 timeOptions& Time_,
+				 pdfField& pdf_,
+				 const scalarField& rho_,
+				 const vectorField& U_,
+				 scalarField& T_) {
+
+    
+    // Energy MRT equation
+
+    EEquation EEq;
+
+    _equation = EEq.create(name, mesh_, Time_, pdf_, rho_, U_, T_);
+
+
+
+    // Read Boundary conditions
+
+    dictionary dict("start/boundaries");
+
+    const map< string, vector<uint> >& bnd = mesh_.boundaries();
+
+    energyBndCreator BndCreator;
+
+    for(map< string, vector<uint> >::const_iterator iter = bnd.begin(); iter != bnd.end(); ++iter)  {
+	
+    	string bdname = iter->first;
+
+    	_boundaries.push_back(   BndCreator.create(name, bdname, mesh_.boundaryNodes(bdname), rho_, T_, U_, pdf_)   );
+
+    }
+    
+
+}
+
+
+
+/** Destructor */
+
+energyEqHandler::~energyEqHandler() {}
