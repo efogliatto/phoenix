5a445a3b14264c64fffcbe22eaf188857f8c43fa
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2019-03-08 16:04:36 -0300

    C\'alculo de NEBB para D2Q9 en los cuatro tipos de frontera Se agreg\'o m\'etodo a pseudoPotEquation para obtener fuerza total en el nodo.

--- src/lbEquations/pseudopotential/boundary/ppNEBB.C
+++ src/lbEquations/pseudopotential/boundary/ppNEBB.C
@@ -31,134 +31,129 @@ ppNEBB::~ppNEBB() {}
 
 void ppNEBB::update( const pseudoPotEquation* ppeq ) {
 
-    cout << "No implementado" << endl;
+
+    // Lattice constants
     
-    exit(1);
+    const uint q = _mesh.lmodel()->q();
 
-}
+    vector<scalar> f_eq(q);
 
-
-
-// /** Update pdf field */
-
-// void ppNEBB::update( const pseudoPotEquation* ppeq ) {
-
-    
-//     // Lattice constants
-    
-//     const uint q = _mesh.lmodel()->q();
-
-//     vector<scalar> f_eq_nb(q);
-
-//     vector<scalar> f_eq_bnd(q);
-
-//     const vector< vector<int> >& nb = _mesh.nbArray();
-
-//     const vector<uint> reverse = _mesh.lmodel()->reverse();
-
-//     const vector< vector<int> > vel = _mesh.lmodel()->lvel();
     
 
 
-//     // Move over boundary elements
+    // Move over boundary elements
 
-//     for( uint i = 0 ; i < _nodes.size() ; i++ ) {
+    for( uint i = 0 ; i < _nodes.size() ; i++ ) {
+
+
+	vector<scalar> Uw = { _bndVal[i][0], _bndVal[i][1], _bndVal[i][2] };
+
+    	uint id = _nodes[i];
+
+	scalar rhow(0);
+	
+	scalar Ft[3] = {0,0,0};
 	
 
-//     	uint id = _nodes[i];
+	// Hand coded
 
-//     	vector<scalar> Uw = { _bndVal[i][0], _bndVal[i][1], _bndVal[i][2] };
+	switch( _mesh.lmodel()->type() ) {
 
-// 	vector<scalar> nvel = { 0,0,0 };
-
-
-//     	for( uint k = 0 ; k < q ; k++ ) {
+	case latticeModel::latticeType::D2Q9:
 
 
-//     	    if ( nb[id][k] == -1 ) {
+	    // Total force at node
+	
+	    ppeq->totalForce(Ft, id);
+	
 
-		
-//     		// Need density and velocity at neighbour (reverse) node
-		    
-//     		int nbid = nb[id][ reverse[k] ];
+	    switch( _normal[i] ) {
 
-
-//     		if( nbid != -1 ) {
-
-
-//     		    // Density and velocity at neighbour node
-		    
-//     		    scalar nbrho = ppeq->localDensity(nbid);
-
-// 		    ppeq->localVelocity(nvel, nbid, true);
-
+	    case latticeMesh::normalType::Y1:
 	    
-	    			
-//     		    // Equilibrium
+		rhow = ppeq->localDensityWithUnknowns( id, _normal[i] );
 
-//     		    ppeq->eqPS( f_eq_nb, nbrho, nvel );
+		ppeq->eqPS( f_eq, rhow, Uw );
 
-//     		    ppeq->eqPS( f_eq_bnd, nbrho, Uw );
-		    
-		    
+		_pdf[id][4] = _pdf[id][2] + (f_eq[4] - f_eq[2]);
 
-//     		    // Update distribution
-			
-//     		    _pdf.set(id, k, f_eq_bnd[k] + (_pdf[nbid][k] - f_eq_nb[k] ) );
+		_pdf[id][7] = _pdf[id][5] + 0.5 * (  -rhow*Uw[0] +  _pdf[id][1] - _pdf[id][3] + f_eq[7] - f_eq[5] + f_eq[8] - f_eq[6] + 0.5*Ft[0]  );
 
-//     		}
-		
-
-//     	    }
-
-
-
-//             // Correction for velocities lying on face
+		_pdf[id][8] = _pdf[id][6] + 0.5 * (   rhow*Uw[0] -  _pdf[id][1] + _pdf[id][3] + f_eq[7] - f_eq[5] + f_eq[8] - f_eq[6] - 0.5*Ft[0]  );
 	    
-// 	    else {
-
-// 		if( nb[id][reverse[k]] != -1 ) {
-		
-// 		    if( q == 9 ) {
-
-// 			if( ( k == 1 )  ||  ( k == 2 )  ||  ( k == 3 )  ||  ( k == 4 )  ) {
-
-// 			    _pdf[id][k] = 0.5 * _pdf[id][k]  +  0.5 * _pdf[id][reverse[k]];
-		
-// 			    _pdf[id][reverse[k]] = _pdf[id][k];
-
-// 			}
-
-// 		    }
+		break;
 
 
-
-// 		    else {
-
-// 		    	if( q == 15 ) {
-
-// 		    	    if( ( k == 1 )  ||  ( k == 2 )  ||  ( k == 3 )  ||  ( k == 4 )   ||  ( k == 5 )   ||  ( k == 6 ) ) {
-
-// 		    		_pdf[id][k] = 0.5 * _pdf[id][k]  +  0.5 * _pdf[id][reverse[k]];
-		
-// 		    		_pdf[id][reverse[k]] = _pdf[id][k];
-
-// 		    	    }
-
-// 		    	}
-
-// 		    }
-
-		    
-
-// 		}
-
-// 	    }
+	    case latticeMesh::normalType::Y0:
 	    
+		rhow = ppeq->localDensityWithUnknowns( id, _normal[i] );
+
+		ppeq->eqPS( f_eq, rhow, Uw );
+
+		_pdf[id][2] = _pdf[id][4] + (f_eq[2] - f_eq[4]);
+
+		_pdf[id][5] = _pdf[id][7] + 0.5 * (   rhow*Uw[0] -  _pdf[id][1] + _pdf[id][3] - f_eq[7] + f_eq[5] - f_eq[8] + f_eq[6] - 0.5*Ft[0]  );
+
+		_pdf[id][6] = _pdf[id][8] + 0.5 * (  -rhow*Uw[0] +  _pdf[id][1] - _pdf[id][3] - f_eq[7] + f_eq[5] - f_eq[8] + f_eq[6] + 0.5*Ft[0]  );
+		
+		break;
+
+
+	    case latticeMesh::normalType::X0:
+
+		rhow = ppeq->localDensityWithUnknowns( id, _normal[i] );
+
+		ppeq->eqPS( f_eq, rhow, Uw );
+
+		_pdf[id][1] = _pdf[id][3] + (f_eq[1] - f_eq[3]);
+
+		_pdf[id][5] = _pdf[id][7] + 0.5 * (   rhow*Uw[1] -  _pdf[id][2] + _pdf[id][4] + f_eq[5] - f_eq[7] + f_eq[8] - f_eq[6] - 0.5*Ft[1]  );
+
+		_pdf[id][8] = _pdf[id][6] + 0.5 * (  -rhow*Uw[1] +  _pdf[id][2] - _pdf[id][4] + f_eq[5] - f_eq[7] + f_eq[8] - f_eq[6] + 0.5*Ft[1]  );		
+	    
+	    	break;
+
+
+	    case latticeMesh::normalType::X1:
+
+		rhow = ppeq->localDensityWithUnknowns( id, _normal[i] );
+
+		ppeq->eqPS( f_eq, rhow, Uw );
+
+		_pdf[id][3] = _pdf[id][1] + (f_eq[3] - f_eq[1]);
+
+		_pdf[id][7] = _pdf[id][5] + 0.5 * (  -rhow*Uw[1] +  _pdf[id][2] - _pdf[id][4] - f_eq[5] + f_eq[7] - f_eq[8] + f_eq[6] + 0.5*Ft[1]  );
+
+		_pdf[id][6] = _pdf[id][8] + 0.5 * (   rhow*Uw[1] -  _pdf[id][2] + _pdf[id][4] - f_eq[5] + f_eq[7] - f_eq[8] + f_eq[6] - 0.5*Ft[1]  );
+	    
+	    	break;	    
 	    
 
-//     	}
+	    default:
 
-//     }
+		cout << "Unable to compute local density" << endl;
+
+		exit(1);
+	    
+		break;
+
+	    }
+
+	
+	    break;
+
+
+	default:
+
+	    cout << "Non-equilibrium bounce-back not yet implemented for this lattice type" << endl;
+
+	    exit(1);
+
+	    break;
+	
+	}
+	
+
+    }
     
-// }
+}
