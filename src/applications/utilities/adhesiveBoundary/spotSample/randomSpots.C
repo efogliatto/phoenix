3f91e700bbd115c2c1cbc9c4af69cbdd7aaba276
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2019-09-30 16:18:49 -0300

    Asignaci\'on de spots

--- src/applications/utilities/adhesiveBoundary/spotSample/randomSpots.C
+++ src/applications/utilities/adhesiveBoundary/spotSample/randomSpots.C
@@ -1,8 +1,29 @@
 #include <randomSpots.H>
 
+#include <random>
+
+
+using namespace std;
+
+
 /** Constructor */
 
-randomSpots::randomSpots(const std::string& bdname) : spotSample(bdname) {}
+randomSpots::randomSpots(const std::string& bdname) : spotSample(bdname) {
+
+    
+    // Distribute locations randomly according node indices
+
+    std::random_device rd;
+    
+    std::mt19937 eng(rd()); 
+
+    std::uniform_int_distribution<> distr(0, _nodes.size()-1); 
+    
+
+    for(uint i = 0 ; i < _spots.size() ; i++)
+    	_spots[i].first = distr(eng);
+
+}
 
 
 /** Destructor */
--- src/applications/utilities/adhesiveBoundary/spotSample/uniformSpots.C
+++ src/applications/utilities/adhesiveBoundary/spotSample/uniformSpots.C
@@ -1,8 +1,22 @@
 #include <uniformSpots.H>
 
+
+using namespace std;
+
+
 /** Constructor */
 
-uniformSpots::uniformSpots(const std::string& bdname) : spotSample(bdname) {}
+uniformSpots::uniformSpots(const std::string& bdname) : spotSample(bdname) {
+
+    
+    // Distribute locations uniformly according node indices
+
+    uint step = (uint)( _nodes.size() / ( _spots.size()+1 ) );
+
+    for(uint i = 0 ; i < _spots.size() ; i++)
+    	_spots[i].first = _nodes[ (i+1)*step ];
+
+}
 
 
 /** Destructor */
