bf443a735163b05719b45e82f5ced6d3eed7485b
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2019-10-01 16:43:28 -0300

    Separaci\'on en procesos y escritura. Hay que revisar el uso de nodos fantasma

--- src/applications/utilities/adhesiveBoundary/adhesiveBoundary.C
+++ src/applications/utilities/adhesiveBoundary/adhesiveBoundary.C
@@ -39,9 +39,86 @@ int main( int argc, char **argv ) {
 
 	vector< pair<uint, scalar> > Gads = spots->computeAds();
 
-	for(auto g : Gads)
-	    cout << g.first << "  " << g.second << endl;
 
+	
+
+	// Distribute over processors
+
+	dictionary pdict("properties/parallel");
+
+	uint np = (uint)pdict.lookUp<scalar>("numProc");
+
+	
+	ifstream infile;
+
+	infile.open("lattice/points");
+
+	uint npoints;
+
+	infile >> npoints;
+
+	infile.close();
+
+
+	
+	infile.open( ("lattice/lattice.graph.part." + to_string(np)).c_str()  );
+
+	if( infile.is_open() == false ){
+	
+	    cout << " [ERROR]  Unable to find file lattice/lattice.graph.part." << np << endl;
+	
+	    exit(1);
+	
+	}	
+
+	vector< map<uint, uint> > globalToLocal(np);
+
+	vector<uint> pidIds(npoints);
+
+	vector<uint> total(np);
+
+	std::fill( total.begin(), total.end(), 0 );
+
+
+	for( uint i = 0 ; i < npoints ; i++ ) {
+
+	    uint pid;
+	    
+	    infile >> pid;
+
+	    pidIds[i] = pid;
+
+	    total[pid]++;
+	    
+	    globalToLocal[pid][i] = total[pid] - 1;
+
+	}
+       
+	infile.close();
+	
+
+	ofstream outfile;
+	
+	for( uint pid = 0 ; pid < np ; pid++ ) {
+
+	    outfile.open( ("processor" + to_string(pid) + "/lattice/" + bdname + "_gads").c_str()  );
+
+
+	    for( auto g : Gads ) {
+
+		if(pidIds[g.first] == pid) {
+
+		    outfile << globalToLocal[pid][g.first] << " " << g.second << endl;
+
+		}
+
+	    }
+	    
+	    
+	    outfile.close();
+
+	}
+	
     }
 
 
