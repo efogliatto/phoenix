d7d18362c4e2123202845363bff6d85fc281918c
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2019-11-27 16:33:13 -0300

    Generalizacion en GeneralNEBB: el cambio de \'indices no se hace a mano, sino que se calcula mediante una matriz de rotaci\'on. Probado para D2Q9 con frontera calefaccionada en Y0 Implementaci\'on de GeneralNEBB para D3Q15

--- src/lbEquations/pseudopotential/boundary/ppGeneralNEBB.C
+++ src/lbEquations/pseudopotential/boundary/ppGeneralNEBB.C
@@ -1,6 +1,33 @@
 #include <ppGeneralNEBB.H>
 
+#include <math.h>
+
 using namespace std;
+
+
+
+// Rotation matrix along unitary vector
+
+const vector< vector<scalar> > ppGeneralNEBB::rotationMatrix( const scalar u, const scalar v, const scalar w, const scalar theta ) const {
+
+    vector< vector<scalar> > _rot = {{0,0,0},{0,0,0},{0,0,0}};
+
+    scalar ct( cos(theta) );
+    scalar st( sin(theta) );
+    
+    _rot[0][0] = ct + u*u*(1-ct);
+    _rot[0][1] = u*v*(1-ct) - w*st;
+    _rot[0][2] = u*w*(1-ct) + v*st;
+    _rot[1][0] = u*v*(1-ct) + w*st;
+    _rot[1][1] = ct + v*v*(1-ct);
+    _rot[1][2] = v*w*(1-ct)-u*st;
+    _rot[2][0] = u*w*(1-ct) - v*st;
+    _rot[2][1] = v*w*(1-ct) + u*st;
+    _rot[2][2] = ct + w*w*(1-ct);
+    
+    return _rot;
+
+}
 
 
 
@@ -23,27 +50,27 @@ ppGeneralNEBB::ppGeneralNEBB( const std::string& eqName,
 
     case latticeModel::latticeType::D2Q9:
 
-	trIndex[ latticeMesh::normalType::Y0 ] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-	trIndex[ latticeMesh::normalType::Y1 ] = { 0, 3, 4, 1, 2, 7, 8, 5, 6 };
-	trIndex[ latticeMesh::normalType::X0 ] = { 0, 4, 1, 2, 3, 8, 5, 6, 7 };
-	trIndex[ latticeMesh::normalType::X1 ] = { 0, 2, 3, 4, 1, 6, 7, 8, 5 };
-
-	prDirIndex[ latticeMesh::normalType::Y0 ] = {  1, 2 };
-	prDirIndex[ latticeMesh::normalType::Y1 ] = {  1, 2 };
-	prDirIndex[ latticeMesh::normalType::X0 ] = {  2, 1 };
-	prDirIndex[ latticeMesh::normalType::X1 ] = {  2, 1 };
-
-	prDirSign[ latticeMesh::normalType::Y0 ] = {  1,  1 };
-	prDirSign[ latticeMesh::normalType::Y1 ] = { -1, -1 };
-	prDirSign[ latticeMesh::normalType::X0 ] = { -1,  1 };
-	prDirSign[ latticeMesh::normalType::X1 ] = {  1, -1 };	
+	rotation[ latticeMesh::normalType::X0 ] = rotationMatrix(0,0,1,M_PI/2);
+	rotation[ latticeMesh::normalType::X1 ] = rotationMatrix(0,0,1,-M_PI/2);
+	rotation[ latticeMesh::normalType::Y0 ] = rotationMatrix(0,0,0,0);
+	rotation[ latticeMesh::normalType::Y1 ] = rotationMatrix(0,0,1,M_PI);
+	
+	// trIndex[ latticeMesh::normalType::Y0 ] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+	// trIndex[ latticeMesh::normalType::Y1 ] = { 0, 3, 4, 1, 2, 7, 8, 5, 6 };
+	// trIndex[ latticeMesh::normalType::X0 ] = { 0, 4, 1, 2, 3, 8, 5, 6, 7 };
+	// trIndex[ latticeMesh::normalType::X1 ] = { 0, 2, 3, 4, 1, 6, 7, 8, 5 };
 	
 	break;
 
 
     case latticeModel::latticeType::D3Q15:
 
-
+	rotation[ latticeMesh::normalType::X0 ] = rotationMatrix(0, 0, 0, 0);
+	rotation[ latticeMesh::normalType::X1 ] = rotationMatrix(0, 1, 0, M_PI);
+	rotation[ latticeMesh::normalType::Y0 ] = rotationMatrix(0, 0, 1,-M_PI/2);
+	rotation[ latticeMesh::normalType::Y1 ] = rotationMatrix(0, 0, 1, M_PI/2);
+	rotation[ latticeMesh::normalType::Z0 ] = rotationMatrix(0, 1, 0, M_PI/2);
+	rotation[ latticeMesh::normalType::Z1 ] = rotationMatrix(0, 1, 0,-M_PI/2);	
 
 	break;
 
@@ -57,6 +84,78 @@ ppGeneralNEBB::ppGeneralNEBB( const std::string& eqName,
 	break;
 
     }
+
+
+
+
+    // Move over lattice velocities, transform and check velocity indexing
+
+    const vector< vector<int> >& vel = _mesh.lmodel()->lvel();
+
+
+    for( auto const& rot : rotation ) {
+	
+
+    	trIndex[rot.first].resize( _mesh.lmodel()->q(),0 );
+
+	
+    	for( uint k = 0 ; k < _mesh.lmodel()->q() ; k++ ) {
+
+
+    	    // Rotate grid velocity
+	    
+    	    scalar rotVel[3] = {0,0,0};
+
+    	    for( uint i = 0 ; i < 3 ; i++ ) {
+
+    		for( uint j = 0 ; j < 3 ; j++ ) {
+
+    		    rotVel[i] = rotVel[i] + rot.second[i][j]*vel[k][j];
+
+    		}
+
+
+		// Round for better comparison
+		
+		rotVel[i] = std::round( rotVel[i] );
+
+		if( rotVel[i] == -0 )
+		    rotVel[i] = 0;
+
+    	    }
+	   	   
+
+
+
+    	    // Check for grid correspondence
+
+    	    for( uint i = 0 ; i < _mesh.lmodel()->q() ; i++ ) {
+
+    		if( vel[i][0] == (int)rotVel[0] ) {
+		    
+		    if( vel[i][1] == (int)rotVel[1] ) {
+
+			if ( vel[i][2] == (int)rotVel[2] ) {
+			
+			    trIndex[ rot.first ][i] = k;
+
+			}
+
+		    }
+		    
+    		}
+
+    	    }
+
+
+	    
+
+    	}
+
+    }
+
+    
+    
     
 }
 
@@ -88,6 +187,8 @@ void ppGeneralNEBB::update( const pseudoPotEquation* ppeq ) {
     vector<scalar> Uw = {0,0,0};	
 	
     scalar Ft[3] = {0,0,0};
+
+    scalar rotFt[3] = {0,0,0};
 
         
 
@@ -122,12 +223,25 @@ void ppGeneralNEBB::update( const pseudoPotEquation* ppeq ) {
 
 	else {
 	    
-	    // Total force  and velocity at node
+	    // Total force at node
 	
 	    ppeq->totalForce(Ft, id);
 
+	    // for(uint j = 0 ; j < 3 ; j++)	    
+	    // 	Uw[j] = _bndVal[i][j];
+
+
+	    // Rotate force and velocity at node
+
+	    vector< vector<scalar> >& rot = rotation[ _normal[i] ];	    
+
 	    for(uint j = 0 ; j < 3 ; j++)	    
-		Uw[j] = _bndVal[i][j];
+	    	Uw[j] = rot[j][0] * _bndVal[i][0]  +  rot[j][1] * _bndVal[i][1]  +  rot[j][2] * _bndVal[i][2];
+
+	    for(uint j = 0 ; j < 3 ; j++)	    
+	    	rotFt[j] = rot[j][0] * Ft[0]  +  rot[j][1] * Ft[1]  +  rot[j][2] * Ft[2];	    
+
+	    
 
 	    
 	    
@@ -136,32 +250,76 @@ void ppGeneralNEBB::update( const pseudoPotEquation* ppeq ) {
 	    
 	    const vector<uint>& tr = trIndex[ _normal[i] ];
 
-	    const vector<int>& pr = prDirIndex[ _normal[i] ];
+	    scalar rho_w(0);
 
-	    const vector<int>& pr_sign = prDirSign[ _normal[i] ];
+	    scalar Delta[3] = {0,0,0};
+
 
 
 	    switch( _mesh.lmodel()->type() ) {
+		
 	    
 	    case latticeModel::latticeType::D2Q9:
 
 
-		scalar rho_w = ( _pdf[id][tr[0]] + _pdf[id][tr[1]] + _pdf[id][tr[3]] + 2.0*(_pdf[id][tr[4]] + _pdf[id][tr[7]] + _pdf[id][tr[8]]) - 0.5*pr_sign[1]*Ft[pr[1]]  ) / (1.0 - pr_sign[1]*Uw[pr[1]]);
-	    
-		_pdf[id][ tr[2] ] = _pdf[id][ tr[4] ]  +  (2.0/3.0) * rho_w  *  Uw[ pr[1] ] * pr_sign[1];
+		// Wall density
+		
+		rho_w = ( _pdf[id][tr[0]] + _pdf[id][tr[1]] + _pdf[id][tr[3]] + 2.0*(_pdf[id][tr[4]] + _pdf[id][tr[7]] + _pdf[id][tr[8]]) - 0.5*rotFt[1]  ) / (1.0 - Uw[1]);
+
+
+		// Update distribution
+		
+		_pdf[id][ tr[2] ] = _pdf[id][ tr[4] ]  +  (2.0/3.0) * rho_w  *  Uw[1];
 
 		_pdf[id][ tr[5] ] = _pdf[id][ tr[7] ]
-		    + 0.5 * rho_w * pr_sign[0] * Uw[pr[0]]
-		    + (rho_w/6.0) * pr_sign[1] *Uw[pr[1]]
-		    - 0.5 * ( _pdf[id][ tr[1] ] - _pdf[id][ tr[3] ] )
-		    - 0.25 * ( pr_sign[0]*Ft[pr[0]] + pr_sign[1]*Ft[pr[1]] );
+		                  + 0.5 * rho_w * Uw[0]
+		                  + (rho_w/6.0) * Uw[1]
+		                  - 0.5 * ( _pdf[id][ tr[1] ] - _pdf[id][ tr[3] ] )
+		                  - 0.25 * ( rotFt[0] + rotFt[1] );
 
 		_pdf[id][ tr[6] ] = _pdf[id][ tr[8] ]
-		    - 0.5 * rho_w * pr_sign[0] * Uw[pr[0]]
-		    + (rho_w/6.0) * pr_sign[1] *Uw[pr[1]]
-		    + 0.5 * ( _pdf[id][ tr[1] ] - _pdf[id][ tr[3] ] )
-		    - 0.25 * ( -pr_sign[0]*Ft[pr[0]] + pr_sign[1]*Ft[pr[1]] );
+		                  - 0.5 * rho_w * Uw[0]
+		                  + (rho_w/6.0) * Uw[1]
+		                  + 0.5 * ( _pdf[id][ tr[1] ] - _pdf[id][ tr[3] ] )
+		                  - 0.25 * ( -rotFt[0] + rotFt[1] );		
 
+
+		break;
+
+
+
+
+
+	    case latticeModel::latticeType::D3Q15:
+
+
+		// Wall density
+		
+		rho_w = ( _pdf[id][tr[0]] + _pdf[id][tr[3]] + _pdf[id][tr[4]] + _pdf[id][tr[5]] + _pdf[id][tr[6]]  +  2*( _pdf[id][tr[2]] + _pdf[id][tr[8]] + _pdf[id][tr[10]] + _pdf[id][tr[12]] + _pdf[id][tr[14]] )  - 0.5*rotFt[0] )  /  (1 - Uw[0]);
+
+
+		// Momentum correctors
+		
+		Delta[0] = -rotFt[0] / 8.0;
+
+		Delta[1] = -(_pdf[id][tr[3]] - _pdf[id][tr[4]]) / 4.0   +   rho_w * Uw[1] / 6.0   -   rotFt[1] / 8.0;
+
+		Delta[2] = -(_pdf[id][tr[5]] - _pdf[id][tr[6]]) / 4.0   +   rho_w * Uw[2] / 6.0   -   rotFt[2] / 8.0;
+
+
+		// Update distribution
+
+		_pdf[id][tr[1]]  = _pdf[id][tr[2]]     +    2.0 * rho_w * Uw[0] / 3.0;
+
+		_pdf[id][tr[7]]  = _pdf[id][tr[14]]    +    rho_w * ( Uw[0] + Uw[1] + Uw[2] ) / 12.0    +    Delta[0] + Delta[1] + Delta[2];
+
+		_pdf[id][tr[9]]  = _pdf[id][tr[12]]    +    rho_w * ( Uw[0] - Uw[1] + Uw[2] ) / 12.0    +    Delta[0] - Delta[1] + Delta[2];
+
+		_pdf[id][tr[11]] = _pdf[id][tr[10]]    +    rho_w * ( Uw[0] + Uw[1] - Uw[2] ) / 12.0    +    Delta[0] + Delta[1] - Delta[2];
+
+		_pdf[id][tr[13]] = _pdf[id][tr[8]]     +    rho_w * ( Uw[0] - Uw[1] - Uw[2] ) / 12.0    +    Delta[0] - Delta[1] - Delta[2];
+		
+		
 
 		break;
 
@@ -173,105 +331,5 @@ void ppGeneralNEBB::update( const pseudoPotEquation* ppeq ) {
 
     }
 
-
-
-	
-	
-	
-
-    // 	// Hand coded
-
-    // 	switch( _mesh.lmodel()->type() ) {
-
-    // 	case latticeModel::latticeType::D2Q9:
-
-
-    // 	    // Total force at node
-	
-    // 	    ppeq->totalForce(Ft, id);
-	
-
-    // 	    switch( _normal[i] ) {
-
-    // 	    case latticeMesh::normalType::Y1:
-	    
-    // 		_pdf[id][4] = _pdf[id][2];
-
-    // 		_pdf[id][7] = _pdf[id][5] + 0.5 * (  _pdf[id][1] - _pdf[id][3] ) + 0.25*(Ft[0]+Ft[1]);
-
-    // 		_pdf[id][8] = _pdf[id][6] - 0.5 * (  _pdf[id][1] - _pdf[id][3] ) - 0.25*(Ft[0]-Ft[1]);		
-	    
-    // 		break;
-
-
-    // 	    case latticeMesh::normalType::Y0:
-	    	
-    // 		_pdf[id][2] = _pdf[id][4];
-
-    // 		_pdf[id][5] = _pdf[id][7] - 0.5 * (  _pdf[id][1] - _pdf[id][3] ) - 0.25*(Ft[0]+Ft[1]);
-
-    // 		_pdf[id][6] = _pdf[id][8] + 0.5 * (  _pdf[id][1] - _pdf[id][3] ) + 0.25*(Ft[0]-Ft[1]);
-				
-    // 		break;
-
-
-    // 	    case latticeMesh::normalType::X0:
-
-    // 		_pdf[id][1] = _pdf[id][3];
-
-    // 		_pdf[id][5] = _pdf[id][7] + 0.5 * (  _pdf[id][4] - _pdf[id][2] ) + 0.25*(Ft[0]+Ft[1]);
-
-    // 		_pdf[id][8] = _pdf[id][6] - 0.5 * (  _pdf[id][4] - _pdf[id][2] ) + 0.25*(Ft[0]-Ft[1]);
-			    
-    // 	    	break;
-
-
-    // 	    case latticeMesh::normalType::X1:
-
-    // 		_pdf[id][3] = _pdf[id][1];
-
-    // 		_pdf[id][7] = _pdf[id][5] - 0.5 * (  _pdf[id][4] - _pdf[id][2] ) - 0.25*(Ft[0]+Ft[1]);
-
-    // 		_pdf[id][6] = _pdf[id][8] + 0.5 * (  _pdf[id][4] - _pdf[id][2] ) - 0.25*(Ft[0]-Ft[1]);
-		    
-    // 	    	break;	    
-	    
-
-    // 	    default:
-
-    // 		// Apply simple bounce back rule
-
-    // 		for(uint k = 0 ; k < q ; k++) {
-
-    // 		    if ( nb[id][k] == -1 ) {
-
-    // 			_pdf[id][k] = _pdf[id][reverse[k]];
-
-    // 		    }		    
-
-    // 		}
-	    
-    // 		break;
-
-    // 	    }
-
-	
-    // 	    break;
-
-
-	    
-
-    // 	default:
-
-    // 	    cout << "Non-equilibrium bounce-back not yet implemented for this lattice type" << endl;
-
-    // 	    exit(1);
-
-    // 	    break;
-	
-    // 	}
-	
-
-    // }
     
 }
