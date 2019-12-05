96f1cb0728bbd0a0917cd3a202b10e35c324f23f
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2019-12-05 15:44:18 -0300

    Pruebas con CGAL

--- src/applications/mesh/cartesianMesh/cartesianMesh.C
+++ src/applications/mesh/cartesianMesh/cartesianMesh.C
@@ -14,6 +14,12 @@
 #include <dictionary.H>
 
 
+#include <CGAL/IO/STL_reader.h>
+
+// typedef CGAL::Simple_cartesian<double> Kernel;
+// typedef Kernel::Point_2 Point_2;
+// typedef Kernel::Segment_2 Segment_2;
+
 
 
 using namespace std;
@@ -43,6 +49,22 @@ int main(int argc, char** argv) {
 	
     latticeModel* lbmodel = lbm.create(ldict.lookUpOrDefault<string>("LBModel","D2Q9"));
 
+
+
+    // Load main geometry
+
+    std::ifstream input(argv[1], std::ios::binary);
+    std::vector< CGAL::cpp11::array<double,3> > points;
+    std::vector< CGAL::cpp11::array<int,3> > triangles;
+
+   CGAL::read_STL(input, points, triangles);
+
+   std::cout << points.size() << "  "  << triangles.size() << std::endl;
+
+    
+
+
+    
     
 
 
