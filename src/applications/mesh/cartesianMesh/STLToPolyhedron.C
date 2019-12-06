2fe41b50017c7d47b19e5751de5460c3d14c4c76
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2019-12-06 12:03:28 -0300

    Lectura de polyhedron directamente de archivo

--- src/applications/mesh/cartesianMesh/STLToPolyhedron.C
+++ src/applications/mesh/cartesianMesh/STLToPolyhedron.C
@@ -5,59 +5,11 @@
 #include <fstream>
 
 
-
-
-
-
-
-// A modifier creating a triangle with the incremental builder.
-template <class HDS>
-class Build_triangle : public CGAL::Modifier_base<HDS> {
-
-protected:
-
-    HalfedgeDS::Vertex::Point _p0;
-
-    HalfedgeDS::Vertex::Point _p1;
-
-    HalfedgeDS::Vertex::Point _p2;    
-    
-public:
-    
-    Build_triangle( CGAL::cpp11::array<double,3> point_0,
-		    CGAL::cpp11::array<double,3> point_1,
-		    CGAL::cpp11::array<double,3> point_2 )
-
-	:  _p0( point_0[0], point_0[1], point_0[2] ),
-	   _p1( point_1[0], point_1[1], point_1[2] ),
-	   _p2( point_2[0], point_2[1], point_2[2] ) {}	
-
-
-    
-    void operator()( HDS& hds) {
-	
-        // Postcondition: hds is a valid polyhedral surface.
-        CGAL::Polyhedron_incremental_builder_3<HDS> B( hds, true);
-        B.begin_surface( 3, 1, 6);
-        B.add_vertex( _p0 );
-        B.add_vertex( _p1 );
-        B.add_vertex( _p2 );
-        B.begin_facet();
-        B.add_vertex_to_facet( 0);
-        B.add_vertex_to_facet( 1);
-        B.add_vertex_to_facet( 2);
-        B.end_facet();
-        B.end_surface();
-    }
-};
-
-
-
-
-
 using namespace std;
 
-Polyhedron STLToPolyhedron( const string& name ) {
+
+
+Polyhedron STLToPolyhedron( const string& name, vector< vector<double> >& bbox ) {
 
 
     // Load geometry
@@ -73,18 +25,61 @@ Polyhedron STLToPolyhedron( const string& name ) {
     cout << "Reading geometry with " << points.size() << " points and "  << triangles.size() << " triangles" << endl << endl;
 
 
+
+    // Out to OFF format
+
+    ofstream output( (name + ".off").c_str() );
+
+    output << points.size() << " " << triangles.size() << endl;
+
+    for(auto p : points) {
+	
+	output << p[0] << " " << p[1] << " " << p[2] << endl;
+
+
+	// Check bounding box
+	
+	if(p[0] < bbox[0][0])
+	    bbox[0][0] = p[0];
+
+	if(p[0] > bbox[1][0])
+	    bbox[1][0] = p[0];
+
+	if(p[1] < bbox[0][1])
+	    bbox[0][1] = p[1];
+
+	if(p[1] > bbox[1][1])
+	    bbox[1][1] = p[1];
+
+	if(p[2] < bbox[0][2])
+	    bbox[0][2] = p[2];
+
+	if(p[1] > bbox[1][2])
+	    bbox[1][2] = p[2];		
+
+    }
+	
+
+    for(auto t : triangles)
+	output << 3 << " " <<  t[0] << " " << t[1] << " " << t[2] << endl;    
+
+    
     
     // Construct polyhedron from triangles
 
-    Polyhedron P;    
+    Polyhedron P;
 
-    for( auto tr : triangles ) {
+    ifstream P_off( (name + ".off").c_str() );
 
-	Build_triangle<HalfedgeDS> builder_triangle( points[tr[0]], points[tr[1]], points[tr[2]] );
+    P_off >> P;
 
-	P.delegate( builder_triangle );
+    // for( auto tr : triangles ) {
 
-    }
+    // 	Build_triangle<HalfedgeDS> builder_triangle( points[tr[0]], points[tr[1]], points[tr[2]] );
+
+    // 	P.delegate( builder_triangle );
+
+    // }
 
         
     return P;
--- src/applications/mesh/cartesianMesh/cartesianMesh.C
+++ src/applications/mesh/cartesianMesh/cartesianMesh.C
@@ -15,6 +15,12 @@
 
 #include <STLToPolyhedron.H>
 
+#include <createLatticeGrid.H>
+
+#include <createLatticeCells.H>
+
+
+
 
 
 
@@ -33,34 +39,66 @@ int main(int argc, char** argv) {
     cout << "     |   - | -   |  Cartesian Mesh Generator" << endl;
     cout << "     | -   |   - |  " << endl;
     cout << "     o-----o-----o  " << endl << endl;
+    cout << "                    " << endl;    
 
 
 
     
     // Read model name. Use D2Q9 as default
 
-    dictionary ldict("properties/latticeProperties");
+    dictionary propDict("properties/cartesianMeshProperties");
     
     latticeModelCreator lbm;
 	
-    latticeModel* lbmodel = lbm.create(ldict.lookUpOrDefault<string>("LBModel","D2Q9"));
+    latticeModel* lbmodel = lbm.create(propDict.lookUpOrDefault<string>("LBModel","D2Q9"));
 
 
 
     // Load Main Geometry
 
-    Polyhedron P = STLToPolyhedron( "SALOME/Cavity.stl" );
+    vector< vector<double> > bbox = {{0,0,0}, {0,0,0}};
+    
+    Polyhedron P = STLToPolyhedron(  propDict.lookUp<string>("Geometry/name"), bbox  );
+
+
+    
+    // Build base mesh
+
+    uint nx( bbox[1][0] - bbox[0][0] );
+
+    uint ny( bbox[1][1] - bbox[0][1] );
+
+    uint nz( bbox[1][2] - bbox[0][2] );
+
+
+    cout << endl << "Building base grid" << endl;
+	
+    vector< vector<uint> > basePoints;
+
+    createLatticeGrid( basePoints, lbmodel, nx, ny, nz );
+
+
+
+    cout << endl << "Building base cells" << endl;
+	
+    vector< vector<uint> > baseCells;
+
+    createLatticeCells( baseCells, lbmodel, nx, ny, nz );    
 
 
     
 
+    // Check which cell centers are inside Polyhedron
+
+    vector<uint> isInside;
 
     
     
+    
 
 
 
-    cout << "Finished meshing" << endl << endl;
+    cout << endl << "Finished meshing" << endl << endl;
 
    
     
