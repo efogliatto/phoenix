9d0371c61c1a0fd949b5d7ddf6c9d863baa1f9af
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2020-01-16 15:33:19 -0300

    Cavidad lineal: c\'alculo en base a la distancia del centro del spot

--- src/applications/utilities/adhesiveBoundary/cavityModel/linearCavity.C
+++ src/applications/utilities/adhesiveBoundary/cavityModel/linearCavity.C
@@ -1,5 +1,7 @@
 #include <linearCavity.H>
 
+#include <math.h>
+
 using namespace std;
 
 
@@ -31,48 +33,91 @@ const std::vector< std::pair<uint, scalar> > linearCavity::coeffs(const std::vec
     // Move over spots
 
     for( auto sp : spots ) {
-	
 
-    	// Move over nodes until spot is found
+
+
+    	// Move over nodes until spot is found: position of sp.first (node global index) in nodes (boundary array)
 
     	bool find(false);
 
     	uint i(0);
 
-
+	
     	while( (find==false)  &&  (i<nodes.size()) ) {
 	    
     	    if( sp.first == nodes[i] ) {
 		
     		find = true;
 
-		i--;
+    		i--;
+
+    	    }
+
+    	    i++;	    
+
+    	}
+
+
+	
+	
+	
+	// Check for distance and use linear interpolation
+
+	if(find) {
+
+	    
+	    scalar delta = (max_gads - min_gads) / sp.second;
+
+	    for( uint nd = 0 ; nd < loc.size() ; nd++ ) {
+
+		scalar dist = sqrt(   (loc[nd][0] - loc[i][0]) * (loc[nd][0] - loc[i][0])
+		                    + (loc[nd][1] - loc[i][1]) * (loc[nd][1] - loc[i][1])
+				    + (loc[nd][2] - loc[i][2]) * (loc[nd][2] - loc[i][2])   );
+				
+		
+		if( dist <= sp.second ) {
+		    
+		    Gads[nd] = make_pair( nodes[nd], min_gads + delta*dist );
+
+		}
+
 
 	    }
 
-	    i++;	    
-
-    	}
-
-
-
-    	// Assign min_gads to node and neighbours
-
-    	if(find) {
-
-	    scalar delta = (max_gads - min_gads) / sp.second;	    
-	
-    	    for( uint j = 0 ; j <= sp.second ; j++ ) {
-
-    		if( (i+j) < Gads.size() )
-    		    Gads[i+j] = make_pair( nodes[i+j], min_gads + delta*j );
-
-    		if( (i-j) > 0 )
-    		    Gads[i-j] = make_pair( nodes[i-j], min_gads + delta*j );	    
 	    
-    	    }
 
-    	}
+	}
+
+
+
+
+
+	
+
+
+
+
+
+
+    // 	// Assign min_gads to node and neighbours
+
+    // 	if(find) {
+
+    // 	    scalar delta = (max_gads - min_gads) / sp.second;	    
+	
+    // 	    // for( uint j = 0 ; j <= sp.second ; j++ ) {
+
+    // 	    // 	if( (i+j) < Gads.size() )
+    // 	    // 	    Gads[i+j] = make_pair( nodes[i+j], min_gads + delta*j );
+
+    // 	    // 	if( (i-j) > 0 )
+    // 	    // 	    Gads[i-j] = make_pair( nodes[i-j], min_gads + delta*j );	    
+	    
+    // 	    // }
+
+
+    // 	    // Check for distance and use linear interpolation
+
 	
 
     }
